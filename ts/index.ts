4c6759def5639c0b4d32f6cb5f38835fec96b7f8
Kenneth Reilly <kenneth.reilly@hotmail.com>
2016-11-05 21:18:49 -0600

    Added overlays, removed zoom, added swipe

--- ts/index.ts
+++ ts/index.ts
@@ -1,74 +1,164 @@
-﻿
-module TypeScriptGreeter {
+﻿export namespace Diatonic {
 
-    "use strict";
-    export module Application {
+    enum SwipeDirection { UP, DOWN, LEFT, RIGHT }
 
-        let current = 0;
+    export class TouchController {
+       
+       private static lastX: number = 0;
 
-        export function initialize() {
-            document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+       public static onTouchStart(ev: TouchEvent) {
+            
+            TouchController.lastX = ev.touches[0].clientX; 
+            console.log(TouchController.lastX);
         }
 
-        function onDeviceReady() {
+        public static onTouchEnd(ev: TouchEvent) {
+            
+            var currentX: number = ev.changedTouches[0].clientX;
+            console.log(currentX);
+
+            if (currentX > TouchController.lastX && (currentX - TouchController.lastX) > 100) {
+                TouchController.onSwipe(SwipeDirection.RIGHT);
+            }
+
+            if (currentX < TouchController.lastX && (TouchController.lastX - currentX) > 100) {
+                TouchController.onSwipe(SwipeDirection.LEFT);
+            }
+        }
+
+        public static onSwipe(direction: SwipeDirection) { Wheel.rotate(direction) }
+    }
+
+    export class Controls {
+
+        private static btnLeft: HTMLButtonElement;
+
+        private static btnRight: HTMLButtonElement;
+
+        public static init() {
+            
+            Controls.btnLeft = <HTMLButtonElement> document.querySelector('button.left');
+            Controls.btnRight = <HTMLButtonElement> document.querySelector('button.right');
+
+            Controls.btnLeft.onclick = () => { Wheel.rotate(SwipeDirection.LEFT) }
+            Controls.btnRight.onclick = () => { Wheel.rotate(SwipeDirection.RIGHT) }
+        }
+    }
+
+    export class Wheel {
+
+        private static positions: number = 12;
+
+        private static currentPosition: number = 1;
+
+        private static chart: HTMLElement;
+
+        private static _animate: boolean = false;
+
+        private static get animate(): boolean {
+
+            return Wheel._animate;
+        }
+
+        private static set animate(state: boolean) {
+            
+            Wheel._animate = state;
+            
+            if (Wheel._animate) Wheel.chart.classList.add('animate');
+            else Wheel.chart.classList.remove('animate');
+        }
+
+        public static init() {
+
+            Wheel.chart = <HTMLElement>document.querySelector('chart');
+            Wheel.chart.classList.add('animate');
+
+            //Wheel.chart.addEventListener('transitionend', Wheel.reset);
+        }
+
+        public static reset() {
+
+        
+            Wheel.animate = false;
+            Wheel.chart.classList.remove('position-0');
+            Wheel.chart.classList.remove('position-13');
+            Wheel.animate = true;
+        }
+
+        public static rotate(direction: SwipeDirection) {
+
+            Wheel.chart.classList.remove('position-' + Wheel.currentPosition);
+                console.log("Moving from position: " + Wheel.currentPosition);
+
+            switch (direction) {
+                
+                case SwipeDirection.LEFT:
+
+                    if (Wheel.currentPosition == 1) {
+
+                        // Wheel.animate = false;
+                        // Wheel.chart.classList.add('position-0');
+                        // Wheel.animate = true;
+
+                        Wheel.currentPosition = 12;
+                    }
+                    else { 
+                        Wheel.currentPosition--;
+                    }
+                   
+                    break;
+
+                case SwipeDirection.RIGHT:
+
+                    if (Wheel.currentPosition == 12) {
+
+                        // Wheel.animate = false;
+                        // Wheel.chart.classList.add('position-13');
+                        // Wheel.animate = true;
+
+                        Wheel.currentPosition = 1;
+                    }
+                    else {
+                        Wheel.currentPosition++;
+                    }                   
+            }
+
+            console.log("to position: " + Wheel.currentPosition);
+            Wheel.chart.classList.add('position-' + Wheel.currentPosition);
+        }
+    }
+
+    export class App {
+
+        public static initialize() {
+    
+            document.addEventListener('deviceready', App.onDeviceReady.bind(this), false);
+            
+            Controls.init();
+            Wheel.init();
+        }
+        
+        public static  onDeviceReady() {
+
             // Handle the Cordova pause and resume events
-            document.addEventListener('pause', onPause, false);
-            document.addEventListener('resume', onResume, false);
+            document.addEventListener('pause', App.onPause, false);
+            document.addEventListener('resume', App.onResume, false);
+            document.addEventListener('touchstart', TouchController.onTouchStart, false);
+            document.addEventListener('touchend', TouchController.onTouchEnd, false);
 
             // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
             // var element = document.getElementById('geo');
             // element.addEventListener('click', onClick.bind(this), false);
-
-            var positions = 12;
-            var currentPosition = 1;
-
-            var chart = document.querySelector('chart');
-            var btn = <HTMLButtonElement> document.querySelector('button');
-            btn.onclick = () => {
-                
-                chart.classList.remove('position-' + currentPosition);
-                
-                currentPosition = (currentPosition == 12) 
-                    ? 1 
-                    : currentPosition + 1;
-                
-                chart.classList.add('position-' + currentPosition);
-            }
         }
 
-        function createUser(loc: any) {
-           
-        }
-
-        function showGreeter(person: any) {
-            // var element = document.getElementById('hello');
-            // element.textContent = "Hello, " + person.firstname + " " + person.lastname + " @latitude: " + person.latitude;
-        }
-
-        function onClick(args: any) {
-            navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 3000 });
-        }
-
-        function onSuccess(position: any) {
-            createUser(position.coords.latitude);
-            console.log("found location data");
-        }
-
-        function onError(err: any) {
-            createUser("no location data");
-            console.log("location data not available");
-        }
-
-        function onPause() {
+        public static onPause() {
             // TODO: This application has been suspended. Save application state here.
         }
 
-        function onResume() {
+        public static onResume() {
             // TODO: This application has been reactivated. Restore application state here.
         }
     }
 
-    window.onload = () => {
-        Application.initialize();
-    }
+    window.onload = () => { App.initialize(); }
 }
--- www/js/app.js
+++ www/js/app.js
@@ -1,53 +1,126 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var TypeScriptGreeter;
-(function (TypeScriptGreeter) {
-    "use strict";
-    var Application;
-    (function (Application) {
-        var current = 0;
-        function initialize() {
-            document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+var Diatonic;
+(function (Diatonic) {
+    var SwipeDirection;
+    (function (SwipeDirection) {
+        SwipeDirection[SwipeDirection["UP"] = 0] = "UP";
+        SwipeDirection[SwipeDirection["DOWN"] = 1] = "DOWN";
+        SwipeDirection[SwipeDirection["LEFT"] = 2] = "LEFT";
+        SwipeDirection[SwipeDirection["RIGHT"] = 3] = "RIGHT";
+    })(SwipeDirection || (SwipeDirection = {}));
+    var TouchController = (function () {
+        function TouchController() {
         }
-        Application.initialize = initialize;
-        function onDeviceReady() {
-            document.addEventListener('pause', onPause, false);
-            document.addEventListener('resume', onResume, false);
-            var positions = 12;
-            var currentPosition = 1;
-            var chart = document.querySelector('chart');
-            var btn = document.querySelector('button');
-            btn.onclick = function () {
-                chart.classList.remove('position-' + currentPosition);
-                currentPosition = (currentPosition == 12)
-                    ? 1
-                    : currentPosition + 1;
-                chart.classList.add('position-' + currentPosition);
-            };
+        TouchController.onTouchStart = function (ev) {
+            TouchController.lastX = ev.touches[0].clientX;
+            console.log(TouchController.lastX);
+        };
+        TouchController.onTouchEnd = function (ev) {
+            var currentX = ev.changedTouches[0].clientX;
+            console.log(currentX);
+            if (currentX > TouchController.lastX && (currentX - TouchController.lastX) > 100) {
+                TouchController.onSwipe(SwipeDirection.RIGHT);
+            }
+            if (currentX < TouchController.lastX && (TouchController.lastX - currentX) > 100) {
+                TouchController.onSwipe(SwipeDirection.LEFT);
+            }
+        };
+        TouchController.onSwipe = function (direction) { Wheel.rotate(direction); };
+        TouchController.lastX = 0;
+        return TouchController;
+    })();
+    Diatonic.TouchController = TouchController;
+    var Controls = (function () {
+        function Controls() {
         }
-        function createUser(loc) {
+        Controls.init = function () {
+            Controls.btnLeft = document.querySelector('button.left');
+            Controls.btnRight = document.querySelector('button.right');
+            Controls.btnLeft.onclick = function () { Wheel.rotate(SwipeDirection.LEFT); };
+            Controls.btnRight.onclick = function () { Wheel.rotate(SwipeDirection.RIGHT); };
+        };
+        return Controls;
+    })();
+    Diatonic.Controls = Controls;
+    var Wheel = (function () {
+        function Wheel() {
         }
-        function showGreeter(person) {
+        Object.defineProperty(Wheel, "animate", {
+            get: function () {
+                return Wheel._animate;
+            },
+            set: function (state) {
+                Wheel._animate = state;
+                if (Wheel._animate)
+                    Wheel.chart.classList.add('animate');
+                else
+                    Wheel.chart.classList.remove('animate');
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Wheel.init = function () {
+            Wheel.chart = document.querySelector('chart');
+            Wheel.chart.classList.add('animate');
+        };
+        Wheel.reset = function () {
+            Wheel.animate = false;
+            Wheel.chart.classList.remove('position-0');
+            Wheel.chart.classList.remove('position-13');
+            Wheel.animate = true;
+        };
+        Wheel.rotate = function (direction) {
+            Wheel.chart.classList.remove('position-' + Wheel.currentPosition);
+            console.log("Moving from position: " + Wheel.currentPosition);
+            switch (direction) {
+                case SwipeDirection.LEFT:
+                    if (Wheel.currentPosition == 1) {
+                        Wheel.currentPosition = 12;
+                    }
+                    else {
+                        Wheel.currentPosition--;
+                    }
+                    break;
+                case SwipeDirection.RIGHT:
+                    if (Wheel.currentPosition == 12) {
+                        Wheel.currentPosition = 1;
+                    }
+                    else {
+                        Wheel.currentPosition++;
+                    }
+            }
+            console.log("to position: " + Wheel.currentPosition);
+            Wheel.chart.classList.add('position-' + Wheel.currentPosition);
+        };
+        Wheel.positions = 12;
+        Wheel.currentPosition = 1;
+        Wheel._animate = false;
+        return Wheel;
+    })();
+    Diatonic.Wheel = Wheel;
+    var App = (function () {
+        function App() {
         }
-        function onClick(args) {
-            navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 3000 });
-        }
-        function onSuccess(position) {
-            createUser(position.coords.latitude);
-            console.log("found location data");
-        }
-        function onError(err) {
-            createUser("no location data");
-            console.log("location data not available");
-        }
-        function onPause() {
-        }
-        function onResume() {
-        }
-    })(Application = TypeScriptGreeter.Application || (TypeScriptGreeter.Application = {}));
-    window.onload = function () {
-        Application.initialize();
-    };
-})(TypeScriptGreeter || (TypeScriptGreeter = {}));
+        App.initialize = function () {
+            document.addEventListener('deviceready', App.onDeviceReady.bind(this), false);
+            Controls.init();
+            Wheel.init();
+        };
+        App.onDeviceReady = function () {
+            document.addEventListener('pause', App.onPause, false);
+            document.addEventListener('resume', App.onResume, false);
+            document.addEventListener('touchstart', TouchController.onTouchStart, false);
+            document.addEventListener('touchend', TouchController.onTouchEnd, false);
+        };
+        App.onPause = function () {
+        };
+        App.onResume = function () {
+        };
+        return App;
+    })();
+    Diatonic.App = App;
+    window.onload = function () { App.initialize(); };
+})(Diatonic = exports.Diatonic || (exports.Diatonic = {}));
 
 },{}]},{},[1])
 
